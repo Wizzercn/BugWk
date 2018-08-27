2e6f6f631d9ec57ddf87b8d19e68ae23fa003f4f
Wizzercn <wizzer@qq.com>
2018-08-27 22:45:28 +0800

    赶快写完..

---
+++ vue/src/components/Add.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <el-row >
+            <el-button type="primary" @click="goBack">返回</el-button>
+        </el-row>
+        <el-table
+                :data="tableData"
+                style="width: 100%">
+            <el-table-column
+                    prop="date"
+                    label="日期"
+                    width="180">
+            </el-table-column>
+            <el-table-column
+                    prop="name"
+                    label="姓名"
+                    width="180">
+            </el-table-column>
+            <el-table-column
+                    prop="address"
+                    label="地址">
+            </el-table-column>
+        </el-table>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Add",
+        data() {
+            return {
+                isAdmin:false,
+                tableData: []
+            }
+        },
+        methods: {
+            goBack:function(){
+                this.$router.push("/lol")
+            }
+        },
+        mounted: function () {
+            if("ADMIN"==this.$cookie.get("role")){
+                this.isAdmin=true
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,10 +1,11 @@
 import User from '@/components/User'
 import Lol from '@/components/Lol'
+import Add from '@/components/Add'
 
 export default [
     {
         path: "/",
-        redirect:"lol"
+        redirect: "lol"
     },
     {
         path: "/user",
@@ -15,5 +16,10 @@ export default [
         path: "/lol",
         name: 'lol',
         component: Lol
+    },
+    {
+        path: "/lol/add",
+        name: 'add',
+        component: Add
     },
 ]
